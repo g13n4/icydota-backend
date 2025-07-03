4ed7b43a7a4928f13e187b0528b4640641c04fc9
mx <joey-hzpfywtd@hotmail.com>
2025-07-03 23:14:52 +0300

    add_league

--- scripts/add_league.py
+++ scripts/add_league.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable
+
 import click
 
 from db import get_sync_db_session
@@ -5,11 +7,12 @@ from tasks.league.create_league import get_or_create_league
 
 
 @click.command()
-@click.option('--league_id', '-l', help='League id to add')
-def add_league(league_id: int):
-    db_session = get_sync_db_session(expire=False)
-    get_or_create_league(league_id=league_id, db_session=db_session)
-    print(f"Added league {league_id}")
+@click.option('--league_id', '-l', multiple=True, help='League id to add')
+def add_league(league_id: Iterable[int]):
+    for lid in league_id:
+        db_session = get_sync_db_session(expire=False)
+        get_or_create_league(league_id=lid, db_session=db_session)
+        print(f"Added league {lid}")
 
 
 if __name__ == "__main__":

149dccaf7f597eca2c0ecd22a069c37869ae3481
mx <joey-hzpfywtd@hotmail.com>
2025-07-03 21:09:46 +0300

    delete name

--- scripts/add_league.py
+++ scripts/add_league.py
@@ -8,8 +8,8 @@ from tasks.league.create_league import get_or_create_league
 @click.option('--league_id', '-l', help='League id to add')
 def add_league(league_id: int):
     db_session = get_sync_db_session(expire=False)
-    obj = get_or_create_league(league_id=league_id, db_session=db_session)
-    print(f"Added league {league_id} - {obj.name}")
+    get_or_create_league(league_id=league_id, db_session=db_session)
+    print(f"Added league {league_id}")
 
 
 if __name__ == "__main__":

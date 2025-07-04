4ae97847bb2ce5f5c95e5884101796302264a7a9
mx <joey-hzpfywtd@hotmail.com>
2025-07-04 07:17:45 +0300

    add redis test

---
+++ scripts/test_redis.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+from redis_app import get_redis_single
+
+
+load_dotenv()
+
+REDIS_ADDRESS = os.getenv('REDIS_ADDRESS')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+
+def test_redis():
+    print(f"Testing redis connection...")
+    print(f"Settings set: {REDIS_ADDRESS}:{6379} (password: {REDIS_PASSWORD})")
+    r = get_redis_single()
+
+    key = "hello"
+    value = "world"
+
+    r.set(key, value, ex=60)
+
+    output = r.get(key)
+    assert output == key
+    r.delete(key)
+
+
+if __name__ == "__main__":
+    test_redis()

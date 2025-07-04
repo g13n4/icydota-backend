9caa65b9d39b152441616b0d47a08162a87d5032
mx <joey-hzpfywtd@hotmail.com>
2025-07-04 09:43:58 +0300

    delete processing_tester.py

--- celery_app.py
+++ celery_app.py
@@ -91,9 +91,5 @@ celery_app.conf.beat_schedule = {
         'task': 'process_bad_league_games_(cron)',
         'schedule': crontab(minute='0', hour='*/12'),
     },
-    'test_match': {
-        'task': 'test_match_processing',
-        'schedule': crontab(minute='*/2'),
-    },
 }
 

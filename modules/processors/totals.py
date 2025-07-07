594332c2e6959db78083e87ad21b5bd501cb62fa
mx <joey-hzpfywtd@hotmail.com>
2025-07-07 14:47:53 +0300

    fix normalization

--- modules/processors/totals.py
+++ modules/processors/totals.py
@@ -69,7 +69,7 @@ class TotalPerformanceProcessor:
                         case TotalTeamProcessingOption.AVERAGE:
                             field_value = field_value / field_counter
 
-                    if field_item.pseudo_bool:
+                    if field_item.pseudo_bool and not field_item.team_processing_option:
                         # normalize it
                         setattr(
                             PTD_obj,

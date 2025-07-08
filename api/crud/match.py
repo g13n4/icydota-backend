b1a5fea56b841b4cf072a7e5c9882b6444f7beb8
mx <joey-hzpfywtd@hotmail.com>
2025-07-08 08:07:14 +0300

    query fix

--- api/crud/match.py
+++ api/crud/match.py
@@ -3,6 +3,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from models import Game
 
+
 def _format_name(name: str | None, game_id: int) -> str:
     if name:
         return f"{name} ({game_id})"
@@ -11,16 +12,18 @@ def _format_name(name: str | None, game_id: int) -> str:
 
 
 async def get_games(db_session: AsyncSession, league_id: int):
-    match_objs = await (db_session.exec(
-        select(Game)
-        .where(Game.league_id == league_id, Game.is_broken == False)
-        .order_by(Game.id)
-    ))
+    match_objs = await (
+        db_session.exec(
+            select(Game.id, Game.name)
+            .where(Game.league_id == league_id, Game.is_broken == False)
+            .order_by(Game.id)
+        ))
 
     return {
         "games": [
             {
-                'value': str(match.id),
-                'label': _format_name(match.name, match.id),
-            } for match in match_objs.all()]
+                'value': str(match_id),
+                'label': _format_name(match_name, match_id),
+            } for match_id, match_name in match_objs.all()
+        ]
     }

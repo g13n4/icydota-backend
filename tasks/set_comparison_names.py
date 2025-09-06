91e8fc10272d28283d7701a51bc99d7440a2e2ad
mx <joey-hzpfywtd@hotmail.com>
2025-09-06 15:07:36 +0300

    improve sql

--- tasks/set_comparison_names.py
+++ tasks/set_comparison_names.py
@@ -1,5 +1,3 @@
-from typing import Tuple
-
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from sqlmodel import Session, text
@@ -11,24 +9,12 @@ from db import get_sync_db_session
 logger = get_task_logger(__name__)
 
 
-def set_names(pos_id: int, hero_name: str, player_name: str) -> Tuple[str, str]:
-    return f'{pos_id}/{hero_name}', f'{pos_id}/{hero_name}/{player_name}'
-
-
-@shared_task(name='set_comparison_names', ignore_result=True)
-def set_comparison_names(league_id: None | int = None) -> None:
-    logger.info('Filling names for comparison values')
-
-    db_session: Session = get_sync_db_session(expire=True)
-    db_session.execute(
-        text(
-            """
+def get_sql_req(field_name: str, field_func: str, league_id: int | None = None) -> str:
+    return ("""
             UPDATE comparison_types c_main
-            SET cpd_name_short=CONCAT(pos_cpd.name, '/', h_cpd.name),
-                cps_name_short=CONCAT(pos_cps.name, '/', h_cps.name),
-                cpd_name=CONCAT(pos_cpd.name, '/', h_cpd.name, '/', p_cpd.nickname),
-                cps_name=CONCAT(pos_cps.name, '/', h_cps.name, '/', p_cps.nickname)
-            FROM comparison_types comp_data
+            SET """ + field_func +
+            """
+             FROM comparison_types comp_data
             INNER JOIN performances p ON comp_data.performance_id = p.id
             INNER JOIN players_game_data pgd ON pgd.id = p.player_game_data_id
             INNER JOIN games g ON g.id = pgd.game_id
@@ -40,10 +26,25 @@ def set_comparison_names(league_id: None | int = None) -> None:
             INNER JOIN positions pos_cps ON comp_data.pos_cps_id = pos_cpd.id
             """ +
             f"WHERE performances where type_id = {GamePerformanceTypeConstant.MATCH_DATA_COMPARISON} " +
-            f"AND (c_main.cpd_name_short is null OR c_main.cps_name_short is null OR c_main.cpd_name is null OR c_main.cps_name is null) "
+            f"AND c_main.{field_name} is null "
             f"AND g.league_id = {league_id} " if league_id else ""
-        )
-    )
+            )
+
+
+@shared_task(name='set_comparison_names', ignore_result=True)
+def set_comparison_names(league_id: None | int = None) -> None:
+    logger.info('Filling names for comparison values')
+
+    db_session: Session = get_sync_db_session(expire=True)
+    for field_name, field_func in [
+        ("cpd_name_short", "cpd_name_short=CONCAT(pos_cpd.name, '/', h_cpd.name)"),
+        ("cps_name_short", "cps_name_short=CONCAT(pos_cps.name, '/', h_cps.name)"),
+        ("cpd_name", "cpd_name=CONCAT(pos_cpd.name, '/', h_cpd.name, '/', p_cpd.nickname)"),
+        ("cps_name", "cps_name=CONCAT(pos_cps.name, '/', h_cps.name, '/', p_cps.nickname)"),
+    ]:
+        sql_req = get_sql_req(field_name=field_name, field_func=field_func, league_id=league_id)
+        db_session.execute(text(sql_req))
+        logger.info(f"Setting up {field_name} field")
 
     db_session.full_commit()
     logger.info(f"Comparison name values filled")

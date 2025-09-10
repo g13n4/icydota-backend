eea4220706fa46e892ce6d7648873e279f9dd402
mx <joey-hzpfywtd@hotmail.com>
2025-09-10 04:48:37 +0300

    add fake task

--- tasks/league/process_match.py
+++ tasks/league/process_match.py
@@ -9,6 +9,7 @@ from tasks.game.delete_replay import delete_replay_folder
 from tasks.game.download_replay import get_match_replay
 from tasks.game.process_game import process_game_data
 from tasks.game.single_task_match_processing import single_task_process_game
+from tasks.game.singleton_task import fake_match_task, is_match_locked
 from utils.game_parsers_list import AVAILABLE_PARSERS_PORT
 
 
@@ -23,20 +24,25 @@ def process_game_helper(
         execute: bool = False,
         reason: int | None = None,
 ) -> Optional[chain]:
-    port = next(AVAILABLE_PARSERS_PORT)
-    if MATCH_ONE_TASK == "true":
-        task = single_task_process_game.si(match_id=match_id, league_id=league_id, port=port, reason=reason)
-    else:
-        task = (
-                get_match_replay.si(match_id=match_id, parser_port=port, reason=reason) |
-                process_game_data.si(match_id=match_id, league_id=league_id, reason=reason) |
-                delete_replay_folder.si(match_id=match_id, reason=reason)
-        )
+    is_locked = is_match_locked(match_id=match_id)
 
-    task = task.on_error(
-        create_bad_game_on_error.si(match_id=match_id, league_id=league_id) |
-        delete_replay_folder.si(match_id=match_id)
-    )
+    if is_locked:
+        task = fake_match_task.si()
+    else:
+        port = next(AVAILABLE_PARSERS_PORT)
+        if MATCH_ONE_TASK == "true":
+            task = single_task_process_game.si(match_id=match_id, league_id=league_id, port=port, reason=reason)
+        else:
+            task = (
+                    get_match_replay.si(match_id=match_id, parser_port=port, reason=reason) |
+                    process_game_data.si(match_id=match_id, league_id=league_id, reason=reason) |
+                    delete_replay_folder.si(match_id=match_id, reason=reason)
+            )
+
+        task = task.on_error(
+            create_bad_game_on_error.si(match_id=match_id, league_id=league_id) |
+            delete_replay_folder.si(match_id=match_id)
+        )
 
     if execute:
         task.apply_async()

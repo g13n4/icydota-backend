7d1975e9823b2127f6ac0a40056badc4e9d32a2c
mx <joey-hzpfywtd@hotmail.com>
2025-09-11 14:43:13 +0300

    process league every 10 minutes

--- celery_app.py
+++ celery_app.py
@@ -110,7 +110,7 @@ if AGGREGATION_SEPARATE_TASK == "true":
 celery_app.conf.beat_schedule = {
     'find_leagues_to_process_(cron)_[hourly]': {
         'task': 'find_leagues_to_process_(cron)',
-        'schedule': crontab(minute='0', hour='*/1'),
+        'schedule': crontab(minute='*/10'),
     },
     **strict_cron_time('reprocess_mispositioned_league_games_(cron)', time_start=0, time_step=12),
     'aggregate_and_ccomp_league_(cron)_[at_3]': {
